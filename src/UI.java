46b9d1d8770151e3556d622939814eb7b6bfc37d
Shawn Roberts <Shawn.roberts128@gmail.com>
2016-03-20 01:28:56 -0700

    Testing out GUI stuff

--- src/UI.java
+++ src/UI.java
@@ -1,12 +1,12 @@
 /**
- * Created by llama on 3/15/16.
+ * Created by ::  llama ::  3/15/16.
+ * For :: WeinFilder
  */
 
-import java.awt.EventQueue;
-import javax.swing.JFrame;
+import java.awt.*;
+import javax.swing.*;
 
 public class UI extends JFrame {
-
     // Main
     public static void main(String[] args) {
 
@@ -14,14 +14,18 @@ public class UI extends JFrame {
 
             @Override
             public void run() {
-                Sample ex = new Sample();
+                JFrame.setDefaultLookAndFeelDecorated( false );
+
+                UI ex = new UI(" Whatup ");
+                ex.getContentPane().setBackground( Color.black );
                 ex.setVisible(true);
             }
         });
     }
 
     // The UI class
-    public UI() {
+    public UI(String string) {
+        super(string);
 
         initUI();
     }
@@ -29,10 +33,29 @@ public class UI extends JFrame {
     // init the starts imitation of the window
     private void initUI(){
 
-        setTitle("Test window");
-        setSize(300, 200);
-        setLocationRelativeTo(null);
-        setDefaultCloseOperation(EXIT_ON_CLOSE);
+        this.setDefaultCloseOperation( JFrame.EXIT_ON_CLOSE );
+
+        JLabel label = new JLabel(" What up");
+        JButton clickMe = new JButton("ClickME");
+        clickMe.setBackground( Color.black );
+//        Axis x= new Axis();
+
+        this.getContentPane().add(label);
+        this.getContentPane().add(clickMe);
+//        this.getContentPane().add(x);
+
+
+
+        Dimension screenSize = Toolkit.getDefaultToolkit().getScreenSize();
+        Container c = this.getContentPane();
+        c.setBackground( Color.BLACK );
+        this.getContentPane().setBackground(Color.black);
+        this.repaint();
+        this.pack();
+        this.setSize(screenSize.width/2, screenSize.height/2);
+        this.setLocationRelativeTo(null);
     }
 
+
 }
+
